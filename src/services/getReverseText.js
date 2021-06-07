6ef150f944bbdeeea2a6ea7f535788614d5ddaa0
Marcelo Romitti <romittimarcelo@gmail.com>
2021-06-07 09:46:22 -0300

    styles and refactoring

--- src/services/getReverseText.js
+++ src/services/getReverseText.js
@@ -1,8 +1,9 @@
 export const getReverseText = async( word ) => {
 
-    const url = `http://localhost:4000/iecho?text=${word}`;
+    const url = `${process.env.REACT_APP_URL_API}/iecho?text=${word}`;
     const resp = await fetch( url );
     const data  = await resp.json();
+
     return data;
 
 }
